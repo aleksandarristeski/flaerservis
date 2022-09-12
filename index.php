342e79131a246277b20abedfa1046289443bd943
aleksandarristeski <aleksandar.risteski87@gmail.com>
2022-09-12 11:56:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include ( __DIR__ ) .'/flaer/php/01_head_all.php';
+include ( __DIR__ ) .'/flaer/php/01_header.php';
+include ( __DIR__ ) .'/flaer/php/02_banner.php';
+include ( __DIR__ ) .'/flaer/php/facts.php';
+include ( __DIR__ ) .'/flaer/php/03_services.php';
+include ( __DIR__ ) .'/flaer/php/04_target.php';
+include ( __DIR__ ) .'/flaer/php/05_banner.php';
+include ( __DIR__ ) .'/flaer/php/06_info.php';
+include ( __DIR__ ) .'/flaer/php/06_banner.php';
+include ( __DIR__ ) .'/flaer/php/08_footer.php';
+?>
