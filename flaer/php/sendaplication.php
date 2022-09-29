be855d94c142925c83cd494b1663e1f8cb9dcf92
Aleksandar Risteski <aleksandar.risteski87@gmail.com>
2022-09-29 09:17:02 +0200

    Update sendaplication.php

--- flaer/php/sendaplication.php
+++ flaer/php/sendaplication.php
@@ -1,9 +1,5 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$telefon = $_POST['telefon'];
-$adresa = $_POST['adresa'];
-$poraka = $_POST['poraka'];
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -45,7 +41,6 @@ $mail = new PHPMailer();
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Aplikacija za mini rabota';
-    $mail->Body    = 'Ime i prezime: ' . $name . '<br>Email: ' . $email . '<br>Telefon: ' . $telefon . '<br>Mesto na ziveenje: ' . $adresa . '<br>Poraka: ' .  $poraka ;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Body    = ''
 
     $mail->send();
